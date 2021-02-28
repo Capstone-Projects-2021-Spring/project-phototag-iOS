ea3dc75dd6890afb67b89c0029fa054580ae957c
SebTota <kidokyo1@gmail.com>
2021-02-28 16:14:06 -0500

    Created base xcode application

---
+++ PhotoTag/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PhotoTag
+//
+//  Created by Seb Tota on 2/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Photo Tag")
+    }
+
+
+}
+
