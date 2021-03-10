090baffbda393b6668c650af1e5e0ff9b9167465
Ryan O'Connor <59928691+ryan-SWE@users.noreply.github.com>
2021-03-10 13:12:59 -0500

    Update AppDelegate.swift
    Removed duplicate Firebase configuration

--- PhotoTag/AppDelegate.swift
+++ PhotoTag/AppDelegate.swift
@@ -16,8 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Use Firebase library to configure APIs
         FirebaseApp.configure()
-        // Override point for customization after application launch.
-        FirebaseApp.configure()
         return true
     }
 
