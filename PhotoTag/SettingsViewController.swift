7cca37b9ab601bfe19a97f690ccf071f0b236870
ryan-swe <tu49381@temple.edu>
2021-03-12 16:35:27 -0500

    New view controller for settings

---
+++ PhotoTag/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  PhotoTag
+//
+//  Created by Ryan on 3/12/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var autmaticTagSwitch: UISwitch!
+    
+    @IBOutlet weak var localOrServerSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
