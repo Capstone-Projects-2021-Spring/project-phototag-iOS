5cb9a60b12e6a1e7af5451afb4cb96c728f36615
ryan-swe <tu49381@temple.edu>
2021-03-14 22:36:17 -0400

    Storyboard updates for switches on the settings page

--- PhotoTag/SettingsViewController.swift
+++ PhotoTag/SettingsViewController.swift
@@ -2,18 +2,50 @@
 //  SettingsViewController.swift
 //  PhotoTag
 //
-//  Created by Ryan on 3/14/21.
+//  Created by Ryan on 3/12/21.
 //
 
 import UIKit
+import QuickTableViewController
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController{
 
+    
+    
+    @IBOutlet weak var automaticTaggingLabel: UILabel!
+    @IBOutlet weak var localTaggingLabel: UILabel!
+    
+    @IBOutlet weak var automaticTaggingSwitch: UISwitch!
+    @IBOutlet weak var localTaggingSwitch: UISwitch!
+    
+    var settingsArray: [String] = ["Automatic Tagging", "Local Tagging"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var automaticTagOn  = UserDefaults.standard.bool(forKey: "Autotag")
+        var localTagOn = UserDefaults.standard.bool(forKey: "Localtag")
 
-        // Do any additional setup after loading the view.
+        automaticTaggingSwitch.setOn(automaticTagOn, animated: false)
+        localTaggingSwitch.setOn(localTagOn, animated: false)
+        
     }
+
+
+    @IBAction func automaticTagSwitchChange(_ sender: Any) {
+        var state = UserDefaults.standard.bool(forKey: "Autotag")
+        state = !state
+        UserDefaults.standard.set(state, forKey: "Autotag")
+    }
+    
+    @IBAction func localTagSwitchChange(_ sender: Any) {
+    }
+    
+    
+}
+
+
+
     
 
     /*
@@ -25,5 +57,23 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+   /* @IBAction func switchDidChange(sender: UISwitch){
+        if sender.isOn{
+            view.backgroundColor = .red
+        }
+    }*/
+    /*
+    func numberOfSections(in tV: UITableView) -> Int{
+        return 1
+    }
+    */
 
-}
+    
+
+    
+   /* func tableView(_ tv: UITableView, cellForRowAt section: Int)->{
+        return 2
+    }*/
+
+
