3f5f9680ba59ebe86b2565a44b78562b8e4335f7
ryan-swe <tu49381@temple.edu>
2021-04-06 22:37:33 -0400

    Created scheduletagviewcontroller

--- PhotoTag/ScheduleTagViewController.swift
+++ PhotoTag/ScheduleTagViewController.swift
@@ -5,4 +5,34 @@
 //  Created by Ryan on 4/6/21.
 //
 
-import Foundation
+
+import UIKit
+import Photos
+import FirebaseDatabase
+
+
+
+class ScheduleTagViewController: UIViewController{
+    
+    @IBOutlet weak var fromDatePicker: UIDatePicker!
+    @IBOutlet weak var toDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var saveScheduleButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func saveScheduleButtonDidClick(_ sender: Any) {
+        
+        let scheduleStart = fromDatePicker.date
+        let scheduleEnd = toDatePicker.date
+        print(scheduleStart)
+        
+        //TODO seque view (back to settings?)
+        //TODO add date to server
+        
+    }
+    
+}
