3a30d98b0bab4cd80037484cd3ea121e1fa3425b
SebTota <kidokyo1@gmail.com>
2021-04-08 09:49:34 -0400

    added tag extraction from search string functionality

--- PhotoTag/Search.swift
+++ PhotoTag/Search.swift
@@ -6,3 +6,80 @@
 //
 
 import Foundation
+
+class Search {
+    
+    static let ignoreTerms: [String] = ["show me", "show", "photos of", "photos", "a photo", "including", "at", "in", "on", "the", "my", "we", "you", "i", "while", "during"]
+    static let delimiters: [String] = [",", "and"]
+    
+    static func splitTeremsOnDelim(text: String) -> [String] {
+        var res: [String] = [text]
+        for delim in delimiters {
+            res = res.map { $0.components(separatedBy: delim)}.flatMap { $0 }
+        }
+        return res
+    }
+    
+    static func removeIgnoreTerms(text: String) -> String {
+        var text = text
+        for term in ignoreTerms {
+            text = text.replacingOccurrences(of: " \(term) ", with: " ")
+        }
+        return text
+    }
+    
+    static func termContains(tags: [String], term: String) -> Bool {
+        for tag in tags {
+            if tag.hasPrefix(term) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    static func getTagsFromText(searchText: String, tags: [String]) -> [String] {
+        var searchText = searchText
+        searchText = removeIgnoreTerms(text: searchText)
+//        print("Text after removeIgnoreTerms: \(searchText)")
+
+        let roughSplit = splitTeremsOnDelim(text: searchText)
+//        print("Text after splitTermsOnDelim: \(roughSplit)")
+        
+        var resTags: [String] = []
+        
+        for split in roughSplit {
+            let split: String = split.trimmingCharacters(in: .whitespacesAndNewlines)
+            let words: [String] = split.components(separatedBy: " ")
+            
+//            print("Split being used: \(split)")
+//            print("Words in split: \(words)")
+            
+            var termBuilder: String = ""
+            
+            var i = 0
+            while i < words.count {
+                let word = words[i]
+                
+                let tempTerm = "\(termBuilder) \(word)".trimmingCharacters(in: .whitespacesAndNewlines)
+                if termContains(tags: tags, term: tempTerm) {
+                    termBuilder = tempTerm
+                } else {
+                    if termBuilder == "" {
+                        print("Search: Not a valid tag: \(word)")
+                    } else {
+                        print("Search: Found term: \(termBuilder)")
+                        resTags.append(termBuilder)
+                        i -= 1
+                    }
+                    termBuilder = ""
+                }
+                i += 1
+            }
+            if termBuilder != "" {
+                resTags.append(termBuilder)
+            }
+        }
+        return resTags
+    }
+    
+}
